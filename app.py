9e00fe033f005e2b58e1ec16dc7e27703c727ec9
XXIVCARAT <suhalshetty99@gmail.com>
2026-02-07 14:31:25 +0530

    Gif change

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template_string
+from flask import Flask, url_for, render_template_string
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # 1. UPDATED PLAYERS LIST
-    # Names are in the specific order requested
+    # 1. HARDCODED RANKINGS DATA
     standings_data = [
         {"rank": 1, "name": "Ourab", "elo": 1500},
         {"rank": 2, "name": "Anuj", "elo": 1480},
@@ -17,8 +16,11 @@ def index():
         {"rank": 8, "name": "Ameya", "elo": 1250},
     ]
 
+    # 2. LOAD STATIC FILES
+    # Make sure 'my_cool_gif.gif' is inside the 'static' folder
+    gif_url = url_for('static', filename='my_cool_gif.gif')
+
     # HTML TEMPLATE
-    # Note: This is a standard string (no 'f' at the beginning) to avoid syntax errors.
     html_content = '''
     <!doctype html>
     <html lang="en" data-bs-theme="dark">
@@ -97,7 +99,7 @@ def index():
                             </p>
                             
                             <div class="mt-4">
-                                <img src="https://media.giphy.com/media/l41YkxvU8c7J7Bba0/giphy.gif" alt="Good Luck GIF" class="img-fluid rounded" style="border: 2px solid white; max-width: 100%;">
+                                <img src="{{ gif_url }}" alt="Good Luck GIF" class="img-fluid rounded" style="border: 2px solid white; max-width: 100%;">
                             </div>
                         </div>
 
@@ -112,7 +114,8 @@ def index():
     </html>
     '''
     
-    return render_template_string(html_content, rankings=standings_data)
+    # PASS THE GIF URL HERE
+    return render_template_string(html_content, rankings=standings_data, gif_url=gif_url)
 
 if __name__ == '__main__':
     app.run(debug=True)
